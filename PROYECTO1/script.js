3971d6d25c650104ede34c1dffac066af719e3cf
SebastianAvilaQ <139786192+SebastianAvilaQ@users.noreply.github.com>
2023-10-18 14:53:35 -0500

    script.js

--- PROYECTO1/script.js
+++ PROYECTO1/script.js
@@ -5,44 +5,38 @@ botones.forEach(boton => {
     boton.addEventListener("click", () => {
         const botonOn = boton.textContent;
 
+
         if (boton.id === "limpiar") {
             pantalla.textContent = "0";
             return;
         }
 
+
         if (boton.id === "borrar") {
-            if (pantalla.textContent.length === 1 || pantalla.textContent === "¡Error!") {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === "!ERROR!") {
                 pantalla.textContent = "0";
             } else {
-                pantalla.textContent = pantalla.textContent.slice(0, -1);
+                pantalla.textContent = pantalla.textContent.slice(0, -1)
             }
             return;
+        }
+        if (/[/]/.test(pantalla.textContent.slice(-1)) && /[/]/.test(botonOn)) {
+            throw new Error("ERROR");
         }
 
         if (boton.id === "igual") {
             try {
                 pantalla.textContent = eval(pantalla.textContent);
             } catch {
-                pantalla.textContent = "¡Error!";
+                pantalla.textContent = "!error!"
             }
             return;
         }
-
-        if (pantalla.textContent === "0" || pantalla.textContent === "¡Error!") {
+        if (pantalla.textContent === "0" || pantalla.textContent === "!ERROR!") {
             pantalla.textContent = botonOn;
         } else {
             pantalla.textContent += botonOn;
         }
-        if (pantalla.textContent.includes("/0")) {
-            pantalla.textContent = "¡Error!";
-            return;
-        }
 
-        try {
-            pantalla.textContent = eval(pantalla.textContent);
-        } catch {
-            pantalla.textContent = "¡Error!";
-        }
-    })
-
+    });
 });
