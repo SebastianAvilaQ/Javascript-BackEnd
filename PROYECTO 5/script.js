e2e4bb62443d854257204522b6c49ca1d39abdeb
SebastianAvilaQ <139786192+SebastianAvilaQ@users.noreply.github.com>
2023-10-25 17:33:28 -0500

    script.js

--- PROYECTO 5/script.js
+++ PROYECTO 5/script.js
@@ -4,21 +4,16 @@ let body = document.querySelector("body");
 let botonsonido = new Audio ('C:/Users/Usuario/OneDrive/Escritorio/PROYECTO 5/sound/botonbailar.mp3');
 let botonaudio = new Audio ('C:/Users/Usuario/OneDrive/Escritorio/PROYECTO 5/sound/audio.mp3');
 
-
 function bailar(){
     if (esqueleto == "off"){
         esqueleto = "on"
         esqueletoStop.classList.add("on");
         esqueletoStop.addEventListener('click',() =>{
             botonsonido.play()
-            
         })
         esqueletoStop.addEventListener('click', () =>{
             botonaudio.play()
-            audio.addEventListener("play", function() {
-                body.style.backgroundColor = "red";
-              });
-            
+            body.style.backgroundColor = "rgb(10, 247, 255)";
         })
         console.log("ON");
     }else {
@@ -26,6 +21,7 @@ function bailar(){
         esqueletoStop.classList.remove("on");
         esqueletoStop.addEventListener('click', () =>{
             botonaudio.pause()
+            body.style.backgroundColor = "black";
         })
         console.log("OFF"); 
     }
